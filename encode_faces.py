d32cf3eb2b58e823c684aecf74657882c0fba29c
Christie Tang <60620814+christie-T@users.noreply.github.com>
2024-12-15 00:13:45 -0500

    Update encode_faces.py

--- encode_faces.py
+++ encode_faces.py
@@ -1,10 +1,11 @@
+# code for encode, credit to ravirajsinh45
+
 import face_recognition
 import imutils
 import pickle
 import time
 import cv2
 import os
-
 
 KNOWN_FACES_DIR = 'faces'
 
@@ -23,11 +24,9 @@ for name in os.listdir(KNOWN_FACES_DIR):
             encoding = face_recognition.face_encodings(image)[0]
         except IndexError:
             continue
-        # Append encodings and name
+    
         known_faces.append(encoding)
         known_names.append(name)
-
-
 
 print("[INFO] serializing encodings...")
 data =[known_faces, known_names]
