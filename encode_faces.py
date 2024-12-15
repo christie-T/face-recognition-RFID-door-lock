b9729d10602dd9f7381b410babb303c5b625d4db
Christie Tang <60620814+christie-T@users.noreply.github.com>
2024-12-15 00:08:43 -0500

    Update encode_faces.py

--- encode_faces.py
+++ encode_faces.py
@@ -12,18 +12,13 @@ print('Loading known faces...')
 known_faces = []
 known_names = []
 
-# We oranize known faces as subfolders of KNOWN_FACES_DIR
-# Each subfolder's name becomes our label (name)
 for name in os.listdir(KNOWN_FACES_DIR):
 
-    # Next we load every file of faces of known person
     for filename in os.listdir(f'{KNOWN_FACES_DIR}/{name}'):
         print(f'Processing image of: {name}')
-        # Load an image
+  
         image = face_recognition.load_image_file(f'{KNOWN_FACES_DIR}/{name}/{filename}')
 
-        # Get 128-dimension face encoding
-        # Always returns a list of found faces, for this purpose we take first face only (assuming one face per image as you can't be twice on one image)
         try:
             encoding = face_recognition.face_encodings(image)[0]
         except IndexError:
